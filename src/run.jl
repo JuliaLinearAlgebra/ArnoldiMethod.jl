db12780204e02a487e4f1da1abb5156b142fa9f1
Lauri Nyman <lauri.nyman1@gmail.com>
2018-06-27 03:09:52 +0300

    Apply Givens to whole H

--- src/run.jl
+++ src/run.jl
@@ -2,7 +2,7 @@
 Run IRAM until the eigenvectors are approximated to the prescribed tolerance or until 
 `max_restarts` has been reached.
 """
-function restarted_arnoldi(A::AbstractMatrix{T}, min = 5, max = 30, converged = min, ε = eps(Float64), max_restarts = 10) where {T}
+function restarted_arnoldi(A::AbstractMatrix{T}, min = 5, max = 30, converged = min, ε = eps(T), max_restarts = 10) where {T}
     n = size(A, 1)
 
     arnoldi = initialize(T, n, max)
@@ -14,54 +14,63 @@ function restarted_arnoldi(A::AbstractMatrix{T}, min = 5, max = 30, converged = 
     min′ = min
 
     active = 1
+    λs = sort!(eigvals(view(arnoldi.H, active:max, active:max)), by = abs, rev = true)
     V_new = Matrix{T}(n, min)
     V_prealloc = Matrix{T}(n, min)
     for restarts = 1 : max_restarts
+
         iterate_arnoldi!(A, arnoldi, min′ + 1 : max, h)
-        min′ = implicit_restart!(arnoldi, min, max, active, V_new)
-        new_active = detect_convergence!(view(arnoldi.H, 1:min′+1, 1:min′), ε)
+        min′ = implicit_restart!(arnoldi, λs, min, max, active, V_new)
+        new_active = detect_convergence!(view(arnoldi.H, active:min′+1, active:min′), ε)
+        new_active += active - 1 
         # @show vecnorm(A * arnoldi.V[:, 1:min′] - arnoldi.V[:, 1:min′+1] * arnoldi.H[1:min′+1, 1:min′] )
-        # Bring the new locked part oF H into upper triangular form
         if new_active > active + 1
-
-            # H = Q R Q'
-
-            # A V = V H
-            # A V = V Q R Q'
-            # A (V Q) = (V Q) R
-            
-            # V <- V Q
-            # H_right <- Q' H_right
-            # H_lock <- Q' H_lock Q
-            # H_above <- H_above Q
-
-            H_locked = view(arnoldi.H, active : new_active - 1, active : new_active - 1)
-            schur_form = schur(H_locked)
-            # H_locked .= schur_form[1]
-
-            V_locked = view(V_prealloc, :, active : new_active - 1)
-            V_locked = view(arnoldi.V, :, active : new_active - 1)
-            # H_right = view(arnoldi.H, active : new_active - 1, new_active : min′)
-            H_right = view(arnoldi.H, active : new_active - 1, active : min′)
-
-            A_mul_B!(V_locked, copy(V_locked), schur_form[2])
-            Ac_mul_B!(H_right, schur_form[2], copy(H_right))
-            
-            # if active > 1
-                # H_above = view(arnoldi.H, 1 : active - 1, active : new_active - 1)
-                H_above = view(arnoldi.H, 1 : new_active - 1, active : new_active - 1)
-                A_mul_B!(H_above, copy(H_above), schur_form[2])
-            # end
+            # Bring the new locked part oF H into upper triangular form
+            transform_converged(arnoldi, active, new_active, min′, V_prealloc)
         end
 
         active = new_active
 
+        λs = sort!(eigvals(view(arnoldi.H, active:max, active:max)), by = abs, rev = true)
+
         @show active
 
-        if active >= converged
+        if active > converged
             break 
         end
     end
 
     return PartialSchur(arnoldi.V, arnoldi.H, active - 1)
+end
+
+"""
+Transfrom the converged block into an upper triangular form.
+"""
+function transform_converged(arnoldi, active, new_active, min′, V_prealloc)
+    
+    # H = Q R Q'
+
+    # A V = V H
+    # A V = V Q R Q'
+    # A (V Q) = (V Q) R
+    
+    # V <- V Q
+    # H_right <- Q' H_right
+    # H_lock <- Q' H_lock Q
+    # H_above <- H_above Q
+
+    H_locked = view(arnoldi.H, active : new_active - 1, active : new_active - 1)
+    schur_form = schur(H_locked)
+
+    V_locked = view(V_prealloc, :, active : new_active - 1)
+    V_locked = view(arnoldi.V, :, active : new_active - 1)
+
+    H_right = view(arnoldi.H, active : new_active - 1, active : min′)
+
+    A_mul_B!(V_locked, copy(V_locked), schur_form[2])
+    Ac_mul_B!(H_right, schur_form[2], copy(H_right))
+    
+    H_above = view(arnoldi.H, 1 : new_active - 1, active : new_active - 1)
+    A_mul_B!(H_above, copy(H_above), schur_form[2])
+
 end
