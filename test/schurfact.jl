0f151dacd5a8e287591c8aa881e39353dd86ce59
Lauri Nyman <lauri.nyman1@gmail.com>
2018-07-10 14:24:55 +0300

    Some tests

--- test/schurfact.jl
+++ test/schurfact.jl
@@ -39,4 +39,26 @@ using IRAM: mul!, Givens, Hessenberg, ListOfRotations, qr!, implicit_restart!, i
     @test sort!(eigvalues(H), by = abs, rev = true) ≈ sort!(eigvals(H_copy), by = abs, rev = true)
     @test H[2,1] < 1e-8
     
+    H1 = triu(rand(10,10), -1)
+    @show eigvals(H)
+    H_copy = copy(H1)
+    Q1 = eye(10)
+    schurfact!(H1, Q1, 1, 10)
+    display(H1)
+    @test vecnorm(Q1*H1*Q1' - H_copy) < 1e-8
+
+    H2 = [ 31.3265       -4.09316       1.95146      -0.770332     -0.220247      0.451145;
+    0.133979     30.4833       -2.25368       0.70086      -0.456425      0.255826;   
+    0.0           0.255179     29.8498       -1.13323       0.21986      -0.157696;      
+    3.38813e-21   8.67362e-19   0.410006     29.1263       -0.239704      0.0348652;     
+    2.97702e-22   0.0          -1.73472e-18   0.447611     29.0287       -0.216972;      
+    0.0           5.36667e-22   0.0           1.73472e-18   0.549446     29.2115]
+
+    @show eigvals(H2)
+    H_copy2 = copy(H2)
+    Q2 = eye(6)
+    schurfact!(H2, Q2, 1, 6)
+    # display(H2)
+    @test vecnorm(Q2*H2*Q2' - H_copy2) < 1e-8
+
 end
