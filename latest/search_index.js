e30cc2180e26d9e560dec7e54f94d7e1e4ae825e
autodocs <autodocs>
2018-07-12 09:06:53 +0000

    build based on c37f198

---
+++ latest/search_index.js
@@ -0,0 +1,11 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": "Implicitly restarted Arnoldi method for nonsymmetric eigenvalue problems."
+},
+
+]}
