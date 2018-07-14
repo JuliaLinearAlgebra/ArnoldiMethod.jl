27a90613e5238a18e9408d058441b941dea21d53
Harmen Stoppels <harmenstoppels@gmail.com>
2018-07-14 23:29:22 +0300

    Benchmark w/ different sizes

--- bench/schur.jl
+++ bench/schur.jl
@@ -1,12 +1,12 @@
 using IRAM, LinearAlgebra
 using BenchmarkTools
 
-function bench_schur()
-    H = triu(rand(30, 30), -1)
-    Q = Matrix{Float64}(I, 30, 30)
-    a = @benchmark IRAM._local_schurfact!(HH, 1, 30) setup = (HH = copy($H))
-    b = @benchmark IRAM.local_schurfact!(HH, QQ, 1, 30) setup = (HH = copy($H); QQ = copy($Q))
-    c = @benchmark IRAM._local_schurfact!(HH, 1, 30, QQ) setup = (HH = copy($H); QQ = copy($Q))
+function bench_schur(n = 30)
+    H = triu(rand(n, n), -1)
+    Q = Matrix{Float64}(I, n, n)
+    a = @benchmark IRAM._local_schurfact!(HH, 1, $n) setup = (HH = copy($H))
+    b = @benchmark IRAM._local_schurfact!(HH, 1, $n, QQ) setup = (HH = copy($H); QQ = copy($Q))
+    c = @benchmark IRAM.local_schurfact!(HH, QQ, 1, $n) setup = (HH = copy($H); QQ = copy($Q))
     d = @benchmark eigvals(HH) setup = (HH = copy($H))
     a, b, c, d
 end
