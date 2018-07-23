6f6ad16cf0db5b86846b87bc9e0c05ae9b735515
Lauri Nyman <lauri.nyman1@gmail.com>
2018-07-23 22:43:41 +0300

    Fix shift test

--- test/compute_shifts.jl
+++ test/compute_shifts.jl
@@ -4,35 +4,36 @@ using IRAM: eigvalues, local_schurfact!, backward_subst!, compute_shifts, initia
 using LinearAlgebra
 
 @testset "Shift computation" begin
-    n = 20
-    # A = rand(ComplexF64, n,n)
-    A = triu(rand(ComplexF64, n,n))
-    for i = 1:n
-        A[i,i] = i
+    for j = 1:20
+        n = 20
+        # A = rand(ComplexF64, n,n)
+        A = triu(rand(ComplexF64, n,n))
+        for i = 1:n
+            A[i,i] = i
+        end
+        max = 10
+        h = Vector{ComplexF64}(undef, max)
+        arnoldi = initialize(ComplexF64, n, max)
+        iterate_arnoldi!(A, arnoldi, 1:max, h)
+
+        shifts = compute_shifts(arnoldi.H, 1, max)
+        vals, vecs = eigen(arnoldi.H[1:max, 1:max])
+
+        # Test whether the shifts are the eigenvalues of H
+        @test sort(vals, by=abs) ≈ sort(shifts, by=abs)
+
+        res = Vector{Float64}(undef,max)
+        for i = 1:max
+            res[i] = norm(A * arnoldi.V[:,1:max] * vecs[:,i] - vals[i] * arnoldi.V[:,1:max] * vecs[:,i])
+        end
+        # @show res
+        perm = sortperm(res, by=abs)
+        vals = vals[perm]
+
+        # Test the order of the shifts
+        @test abs.(vals) ≈ abs.(shifts)
+        @test vals ≈ shifts
     end
-    max = 10
-    h = Vector{ComplexF64}(undef, max)
-    arnoldi = initialize(ComplexF64, n, max)
-    iterate_arnoldi!(A, arnoldi, 1:max, h)
-
-    shifts = compute_shifts(arnoldi.H, 1, max)
-    vals, vecs = eigen(arnoldi.H[1:max, 1:max])
-
-    # Test whether the shifts are the eigenvalues of H
-    @test sort(vals, by=abs) ≈ sort(shifts, by=abs)
-
-    res = Vector{Float64}(undef,max)
-    for i = 1:max
-        res[i] = norm(A * arnoldi.V[:,1:max] * vecs[:,i] - vals[i] * arnoldi.V[:,1:max] * vecs[:,i])
-        # @show vecs[:,i]
-    end
-    @show res
-    perm = sortperm(res, by=abs)
-    vals = vals[perm]
-
-    # Test the order of the shifts
-    @test abs.(vals) ≈ abs.(shifts)
-    @test vals ≈ shifts
 end
 
 # AV = VH
