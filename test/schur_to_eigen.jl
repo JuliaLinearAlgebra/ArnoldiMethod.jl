fc48cc7f482ddebc1ec44bfe1088f2bc18e6e009
Lauri Nyman <lauri.nyman1@gmail.com>
2018-08-14 12:07:06 +0300

    Fix test

--- test/schur_to_eigen.jl
+++ test/schur_to_eigen.jl
@@ -4,13 +4,13 @@ using IRAM: partial_schur, schur_to_eigen
 
 @testset "Schur to eigen" begin
     for T in (Float64, ComplexF64), i in 1:10
-        A = rand(100,100)
+        A = rand(T,100,100)
         ε = 1e-6
         minim, maxim = 25, 35
 
         schur_decomp,  = partial_schur(A, min=minim, max=maxim, nev=minim, tol=eps(real(T)), maxiter=20)
         vals, vecs = schur_to_eigen(schur_decomp)
 
-        @test norm(A*vecs - vals'.*vecs) < ε
+        @test norm(A*vecs - vecs*Diagonal(vals)) < ε
     end
 end
