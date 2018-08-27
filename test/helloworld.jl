d942e2fb6f0179f15e7b833138f6c22c09d8d33a
Harmen Stoppels <harmenstoppels@gmail.com>
2018-08-27 11:00:11 +0300

    Fix a test

--- test/helloworld.jl
+++ test/helloworld.jl
@@ -5,12 +5,15 @@ using Plots
 function helloworld()
     A = randn(500, 500)
     @time λs = eigvals(A)
-    @time schur, prods = partial_schur(A, min=10, max=30, tol=1e-6, maxiter=100, which=SR())
-    θs = eigvals(schur.R)
+    @time schur, prods, converged = partial_schur(A, min=15, max=30, tol=1e-6, maxiter=100, which=SR())
 
+    if !converged
+        @warn "Not converged :("
+    end
+
+    θs = eigvals(schur.R)
     p = scatter(real(λs), imag(λs), label = "All eigenvalues", aspect_ratio = :equal)
     scatter!(real(θs), imag(θs), mark = :+, label = "IRAM")
-
     @show norm(A * schur.Q - schur.Q * schur.R) prods
 
     return A, schur, p
--- test/schur_to_eigen.jl
+++ test/schur_to_eigen.jl
@@ -1,16 +1,21 @@
 using Test
 using LinearAlgebra
 using IRAM: partial_schur, schur_to_eigen
+using Random
 
-@testset "Schur to eigen" begin
-    for T in (Float64, ComplexF64), i in 1:10
-        A = rand(T,100,100)
-        ε = 1e-6
-        minim, maxim = 25, 35
+@testset "Schur to eigen $T take $i" for T in (Float64,ComplexF64), i in 1:10
+    Random.seed!(i)
+    A = randn(T, 100, 100)
+    ε = 1e-7
+    minim, maxim = 10, 20
 
-        schur_decomp,  = partial_schur(A, min=minim, max=maxim, nev=minim, tol=eps(real(T)), maxiter=20)
-        vals, vecs = schur_to_eigen(schur_decomp)
+    schur_decomp, prods, converged = partial_schur(A, min=minim, max=maxim, nev=minim, tol=ε, maxiter=100)
+    @test converged
 
-        @test norm(A*vecs - vecs*Diagonal(vals)) < ε
+    vals, vecs = schur_to_eigen(schur_decomp)
+
+    # This test seems a bit flaky sometimes -- have to dig into it.
+    for i = 1 : minim
+        @test norm(A * vecs[:,i] - vecs[:,i] * vals[i]) < ε * abs(vals[i])
     end
 end
