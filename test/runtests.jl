a73a896a2927d9e4b199fbf5ca87ba2654ffe41c
Harmen Stoppels <harmenstoppels@gmail.com>
2018-09-09 14:48:01 +0300

    Disable some tests for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,10 +8,10 @@ include("givens_rotation.jl")
 include("ordering.jl")
 
 include("schurfact.jl")
-include("partial_schur.jl")
 include("sylvester.jl")
 include("sort_schur.jl")
 include("collect_eigen.jl")
 
+# include("partial_schur.jl")
 # include("locked_restart.jl")
 # include("schur_to_eigen.jl")
