a3670d5413d862f190254413a066c6613c8745a7
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-02-12 14:43:05 -0500

    Update docs/make.jl
    Co-authored-by: Harmen Stoppels <harmenstoppels@gmail.com>

--- docs/make.jl
+++ docs/make.jl
@@ -19,6 +19,6 @@ makedocs(
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
 deploydocs(
-    repo = "github.com/JuliaSparse/KLU.jl.git",
+    repo = "github.com/JuliaLinearAlgebra/ArnoldiMethod.jl.git",
     devbranch="main"
 )
